ac56e55bd42b17012912a27cb4946beecc7edcbd
Aleksandr Melnikov <melnikov@tikhvin.org>
2020-02-26 19:42:56 +0300

    corrected

--- src/js/Character.js
+++ src/js/Character.js
@@ -1,4 +1,20 @@
-const characterTypes = [
+function Character(name, type) {
+  if (name.length < 2 || name.length > 10) {
+    throw new Error('Error in Character: wrong name length');
+  }
+  if (!Character.getAvailableTypeNames().includes(type)) {
+    throw new Error('Error in Character: wrong type');
+  }
+
+  this.name = name;
+  this.type = type;
+  this.level = 1;
+  this.health = 100;
+  this.attack = Character.types.filter((value) => value.typeName === type)[0].attack;
+  this.defence = Character.types.filter((value) => value.typeName === type)[0].defence;
+}
+
+Character.types = [
   {
     typeName: 'Bowman',
     attack: 25,
@@ -31,20 +47,6 @@ const characterTypes = [
   },
 ];
 
-export default function Character(name, type) {
-  if (name.length < 2 || name.length > 10) {
-    throw new Error('Error in Character: wrong name length');
-  }
-  if (!characterTypes.map((characterType) => characterType.typeName).includes(type)) {
-    throw new Error('Error in Character: wrong type');
-  }
+Character.getAvailableTypeNames = () => Character.types.map((type) => type.typeName);
 
-  return {
-    name,
-    type,
-    level: 1,
-    health: 100,
-    attack: characterTypes.filter((value) => value.typeName === type)[0].attack,
-    defence: characterTypes.filter((value) => value.typeName === type)[0].defence,
-  };
-}
+export default Character;
--- src/js/__tests__/Character.test.js
+++ src/js/__tests__/Character.test.js
@@ -9,7 +9,7 @@ test('should return right character', () => {
     attack: 25,
     defence: 25,
   };
-  expect(Character('vasya', 'Bowman')).toEqual(expected);
+  expect(new Character('vasya', 'Bowman')).toMatchObject(expected);
 });
 
 test('should return right character', () => {
@@ -21,7 +21,7 @@ test('should return right character', () => {
     attack: 40,
     defence: 10,
   };
-  expect(Character('vasya', 'Swordsman')).toEqual(expected);
+  expect(new Character('vasya', 'Swordsman')).toMatchObject(expected);
 });
 
 test('should return right character', () => {
@@ -33,7 +33,7 @@ test('should return right character', () => {
     attack: 10,
     defence: 40,
   };
-  expect(Character('vasya', 'Magician')).toEqual(expected);
+  expect(new Character('vasya', 'Magician')).toMatchObject(expected);
 });
 
 test('should return right character', () => {
@@ -45,7 +45,7 @@ test('should return right character', () => {
     attack: 25,
     defence: 25,
   };
-  expect(Character('vasya', 'Undead')).toEqual(expected);
+  expect(new Character('vasya', 'Undead')).toMatchObject(expected);
 });
 
 test('should return right character', () => {
@@ -57,7 +57,7 @@ test('should return right character', () => {
     attack: 40,
     defence: 10,
   };
-  expect(Character('vasyavasya', 'Zombie')).toEqual(expected);
+  expect(new Character('vasyavasya', 'Zombie')).toMatchObject(expected);
 });
 
 test('should return right character', () => {
@@ -69,11 +69,11 @@ test('should return right character', () => {
     attack: 10,
     defence: 40,
   };
-  expect(Character('va', 'Daemon')).toEqual(expected);
+  expect(new Character('va', 'Daemon')).toMatchObject(expected);
 });
 
 test('should throw', () => {
-  expect(() => Character('v', 'Bowman')).toThrow();
-  expect(() => Character('v1234567890', 'Bowman')).toThrow();
-  expect(() => Character('vasya', 'Bowman123')).toThrow();
+  expect(() => new Character('v', 'Bowman')).toThrow();
+  expect(() => new Character('v1234567890', 'Bowman')).toThrow();
+  expect(() => new Character('vasya', 'Bowman123')).toThrow();
 });
